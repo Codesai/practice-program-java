80d51b97987dd874ea2c39c4aec1919f3adfca35
trikitrok <trikitrok@gmail.com>
2018-05-13 22:38:16 +0200

    adding first java kata

---
+++ katas_java/mars-rover-movement/src/test/java/RoverTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class RoverTest {
+
+    @Test
+    public void fix_and_rename_me() {
+        assertThat(true, is(false));
+    }
+}
