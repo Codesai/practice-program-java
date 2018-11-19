bc8f1367ea4726efe43c4f4549aef96935a4f0b0
trikitrok <trikitrok@gmail.com>
2018-11-19 21:54:39 +0100

    adding session 8

---
+++ katas_java/08-outside-in-with-bank-kata/src/test/java/bank/tests/acceptance/PrintingAccountStatementOnConsoleTest.java
@@ -0,0 +1,14 @@
+package bank.tests.acceptance;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class PrintingAccountStatementOnConsoleTest {
+    @Test
+    public void printing_statement_including_deposit_and_withdrawal() {
+        // fix this test
+        assertThat(true, is(false));
+    }
+}
