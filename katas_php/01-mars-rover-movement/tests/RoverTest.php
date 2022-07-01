2faaa4ecad972153f7c70a19920c6ea7485fca70
freyes <fran@codesai.com>
2022-07-01 10:43:23 +0100

    first kata in php

---
+++ katas_php/01-mars-rover-movement/tests/RoverTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class RoverTest extends TestCase
+{
+
+    /** @test */
+    public function fix_and_rename_me() {
+        self::assertThat(true, self::isFalse());
+    }
+}
